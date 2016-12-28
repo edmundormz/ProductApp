7a4d7941d1f5169698e436a9b580c6f3ef2c3cee
H. Edmundo Ramírez <edmundormz@gmail.com>
2016-12-27 21:38:22 -0600

    Added IProductRepository interface

---
+++ ProductApp/ProductApp.core/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+//using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.core.Interfaces
+{
+    interface IProductRepository
+    {
+        void Add(Product p);
+        void Edit(Product p);
+        void Remove(int Id);
+        IEnumerable GetProducts();
+        Product FindById(int Id);
+    }
+}
