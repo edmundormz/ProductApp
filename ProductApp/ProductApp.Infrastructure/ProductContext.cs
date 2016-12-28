c69c9e052bb025c4d70929fe3c98c01a2ce53dd9
H. Edmundo Ramírez <edmundormz@gmail.com>
2016-12-27 21:58:26 -0600

    Added ProductContext class

---
+++ ProductApp/ProductApp.Infrastructure/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ProductApp.Infrastructure
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("name=ProductAppConnectionString") { }
+        public DbSet Products { get; set; }
+    }
+}
